a3dffb932f0f794f86c7f487224600c9249cc52e
Ognjen Stanisavljevic <ognjen.stanisavljevic@thingsolver.com>
2021-04-12 23:05:52 +0200

    added new part for check

--- register-api/src/main.py
+++ register-api/src/main.py
@@ -1,9 +1,10 @@
 import logging
 import requests
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from createuser import CreateUser
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel("DEBUG")
@@ -23,9 +24,9 @@ async def index():
     return {"HEALTH" : "OK"}
 
 @app.post("/register")
-async def newreg(item:Item):
+async def register(item:Item):
+    print(CreateUser(item.email, item.username, item.enabled, item.firstName, item.lastName, item.secret).checker())
     CreateUser(item.email, item.username, item.enabled, item.firstName, item.lastName, item.secret).new_user()
-    return {"REGISTRATION" : "SUCCESSFUL"}
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, loop="asyncio")
