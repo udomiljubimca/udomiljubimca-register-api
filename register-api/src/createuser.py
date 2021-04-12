e30f425e3f4582df77e70346eb3a5aab7b1bbc3f
Ognjen Stanisavljevic <ognjen.stanisavljevic@thingsolver.com>
2021-04-12 23:04:51 +0200

    added checked def

--- register-api/src/createuser.py
+++ register-api/src/createuser.py
@@ -1,7 +1,8 @@
 from keycloak import KeycloakOpenID
 from keycloak import KeycloakAdmin
 import os
-# Configure client
+
+from fastapi import HTTPException
 
 class CreateUser():
     def __init__(self, email, username, enabled, firstName, lastName, secret):
@@ -11,6 +12,29 @@ class CreateUser():
         self.firstName = firstName
         self.lastName = lastName
         self.secret = secret
+    def checker(self):
+        KeycloakOpenID(server_url = "http://nginx/auth/",
+                        client_id = "app",
+                        realm_name = os.getenv('CLIENT_RELM_NAME'),
+                        client_secret_key = os.getenv('CLIENT_RELM_SECRET')
+                        )
+
+        keycloak_admin = KeycloakAdmin(server_url="http://nginx/auth/",
+                                username = os.getenv('KYECLOAK_ADMIN_USER'),
+                                password = os.getenv('KYECLOAK_ADMIN_PASSWORD'),
+                                realm_name = "master",
+                                verify = True)
+        keycloak_admin.realm_name = os.getenv('CLIENT_RELM_NAME')
+
+        users = keycloak_admin.get_users()
+        emails = []
+        for email in users:
+            emails.append(email["email"])
+
+        if self.email in emails:
+            return {"exist": True}
+        else:
+            return {"exist": False}
 
     def new_user(self):
         KeycloakOpenID(server_url = "http://nginx/auth/",
@@ -38,3 +62,5 @@ class CreateUser():
                         }
                     ]
                     })
+        # else:
+        #     return HTTPException(status_code=403, detail="User alredy exist!")
