26a5a6ed2023ac2b20d745efe5a043b9a2ae92fa
mifa43 <mifa43kotez@gmail.com>
2021-04-19 09:47:06 +0200

    nova skripta sa opisom ruta

---
+++ register-api/src/docs.py
@@ -0,0 +1,11 @@
+register_api_docs = [
+
+    {
+        "name" : "Registracija",
+        "description" : "Ovo je ruta ge se kreira user. Informacije o useru se upisuju u json"
+    },
+    {
+        "name" : "Provera zdravlja",
+        "description" : "Ovo je ruta za proveru da li radi api. Ako je radi vraca ok"
+    }
+]
