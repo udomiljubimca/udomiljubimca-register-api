ce9c293dc2480dbf736137c469b3869fba5ebf95
mifa43 <mifa43kotez@gmail.com>
2021-04-25 16:15:48 +0200

    pozvane nove definicije u mainu

--- register-api/src/main.py
+++ register-api/src/main.py
@@ -25,12 +25,18 @@ async def index():
 @app.post("/register", tags = ["Registracija"])
 async def register(item:Item):
     checkerica = CreateUser(item.email, item.username, item.firstName, item.lastName, item.secret).checker()
+    
     if checkerica['exist'] == False:
         CreateUser(item.email, item.username, item.firstName, item.lastName, item.secret).new_user()
+        check_user_id = CreateUser(item.email, item.username, item.firstName, item.lastName, item.secret).get_keycloak_user_id()
+        if check_user_id["exist"] == False:
+            print("user does not exist")
+        else:
+            CreateUser(item.email, item.username, item.firstName, item.lastName, item.secret).verify_email(check_user_id["user_id_keycloak"])
+            print("The email has been successfully sent!")
         return {"message" : "The user has been successfully created!"}
     else:
         raise HTTPException(status_code = 409, detail = "User already exists")
-
-
+    
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, loop="asyncio")
