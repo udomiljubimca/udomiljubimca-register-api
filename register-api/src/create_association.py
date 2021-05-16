bb3b6ac3d9389be8a6b20c62b9b9f964c0f6b8e8
mifa43 <mifa43kotez@gmail.com>
2021-05-16 11:56:57 +0200

    nova definicja koja dodeljuje role

--- register-api/src/create_association.py
+++ register-api/src/create_association.py
@@ -1,13 +1,13 @@
 from keycloak import KeycloakOpenID
 from keycloak import KeycloakAdmin
 import os
-from is_email import Is_email_valid  
+from is_email import Is_email_valid 
 
 class CreateAssociation():
     def __init__(self, email, username_association, secret):
         self.email = email
         self.username_association = username_association
-        self.secret = secret
+        self.secret = secret  
     def create_keycloak_roles(self):  
         keycloak_admin = KeycloakAdmin(server_url="{}/auth/".format(os.getenv('KEYCLOAK_URL')),
                                 username = os.getenv('KEYCLOAK_ADMIN_USER'),
@@ -17,9 +17,19 @@ class CreateAssociation():
         keycloak_admin.realm_name = os.getenv('CLIENT_RELM_NAME')
         client_id = keycloak_admin.get_client_id(os.getenv('KEYCLOAK_CLIENT_NAME'))
         try:
-            create_role = keycloak_admin.create_client_role(client_role_id=client_id, payload={'name': 'association_role', 'clientRole': True}) 
+            create_role = keycloak_admin.create_client_role(client_role_id=client_id, payload={'name': 'association_role', 'clientRole': True})
         except:
-            print("Role with name user_role already exists")
+            print("Role with name association_role already exists")
+       
+    def assign_keycloak_roles(self):
+        keycloak_admin = KeycloakAdmin(server_url="{}/auth/".format(os.getenv('KEYCLOAK_URL')),
+                                username = os.getenv('KEYCLOAK_ADMIN_USER'),
+                                password = os.getenv('KEYCLOAK_ADMIN_PASSWORD'),
+                                realm_name = "master",
+                                verify = True)
+        keycloak_admin.realm_name = os.getenv('CLIENT_RELM_NAME')   
+        
+        client_id = keycloak_admin.get_client_id(os.getenv('KEYCLOAK_CLIENT_NAME'))
         user_id = keycloak_admin.get_user_id(self.username_association)
         role_id = keycloak_admin.get_client_role_id(client_id=client_id, role_name="association_role")
         add_role = keycloak_admin.assign_client_role(user_id, client_id,[{'id' : role_id, 'name':'association_role'}])
