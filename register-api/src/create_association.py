4dda277b8f6e51e59a459f35419105cf95892996
mifa43 <mifa43kotez@gmail.com>
2021-05-16 10:36:14 +0200

    kreirana klasa za kreiranje role i dodelu iste svim novo registrovanim udruzenjima

--- register-api/src/create_association.py
+++ register-api/src/create_association.py
@@ -8,6 +8,22 @@ class CreateAssociation():
         self.email = email
         self.username_association = username_association
         self.secret = secret
+    def create_keycloak_roles(self):  
+        keycloak_admin = KeycloakAdmin(server_url="{}/auth/".format(os.getenv('KEYCLOAK_URL')),
+                                username = os.getenv('KEYCLOAK_ADMIN_USER'),
+                                password = os.getenv('KEYCLOAK_ADMIN_PASSWORD'),
+                                realm_name = "master",
+                                verify = True)
+        keycloak_admin.realm_name = os.getenv('CLIENT_RELM_NAME')
+        client_id = keycloak_admin.get_client_id(os.getenv('KEYCLOAK_CLIENT_NAME'))
+        try:
+            create_role = keycloak_admin.create_client_role(client_role_id=client_id, payload={'name': 'association_role', 'clientRole': True}) 
+        except:
+            print("Role with name user_role already exists")
+        user_id = keycloak_admin.get_user_id(self.username_association)
+        role_id = keycloak_admin.get_client_role_id(client_id=client_id, role_name="association_role")
+        add_role = keycloak_admin.assign_client_role(user_id, client_id,[{'id' : role_id, 'name':'association_role'}])
+
     def get_keycloak_user_id(self):
         keycloak_admin = KeycloakAdmin(server_url="{}/auth/".format(os.getenv('KEYCLOAK_URL')),
                                 username = os.getenv('KEYCLOAK_ADMIN_USER'),
