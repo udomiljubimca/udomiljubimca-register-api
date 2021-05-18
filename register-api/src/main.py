2ab6915c88576c5800c2bb05da2ea72f7a1b8d78
Ognjen Stanisavljevic <ognjen.stanisavljevic@thingsolver.com>
2021-05-18 20:37:50 +0200

    try debug

--- register-api/src/main.py
+++ register-api/src/main.py
@@ -9,6 +9,8 @@ from resend_email import Resend_verify_email
 from typing import Optional
 from create_association import CreateAssociation
 from is_email import Is_email_valid
+from keycloak import KeycloakAdmin
+import os
 
 logger = logging.getLogger(__name__)
 logger.setLevel("DEBUG")
@@ -65,15 +67,20 @@ async def register_association(item:Item_association):
     if checkerica_email['exist'] == True:
         if checkerica['exist'] == False:
             CreateAssociation(item.email, item.username, item.secret).new_association()
-            check_association_id = CreateAssociation(item.email, item.username, item.secret).get_keycloak_user_id()
-            print(check_association_id)
-            # if check_association_id["exist"] == False:
-            #     print("Association cannot be found or does not exist.")
-            # else:
-            #     CreateAssociation(item.email, item.username, item.secret).assign_keycloak_roles()
-            #     CreateAssociation(item.email, item.username, item.secret).verify_email(check_association_id['user_id_keycloak'])
-            #     print("The email has been successfully sent!")
-            #return {"message" : "The association has been successfully created!"}
+            keycloak_admin = KeycloakAdmin(server_url="{}/auth/".format(os.getenv('KEYCLOAK_URL')),
+                                            username = os.getenv('KEYCLOAK_ADMIN_USER'),
+                                            password = os.getenv('KEYCLOAK_ADMIN_PASSWORD'),
+                                            realm_name = "master",
+                                            verify = True)
+            keycloak_admin.realm_name = os.getenv('CLIENT_RELM_NAME')
+            user_id_keycloak = keycloak_admin.get_user_id("associacija")
+            if user_id_keycloak == None:
+                print("Association cannot be found or does not exist.")
+            else:
+                CreateAssociation(item.email, item.username, item.secret).assign_keycloak_roles()
+                CreateAssociation(item.email, item.username, item.secret).verify_email(user_id_keycloak)
+                print("The email has been successfully sent!")
+            return {"message" : "The association has been successfully created!"}
         else:
             raise HTTPException(status_code = 409, detail = "Association already exists")
     else:
