9bd61a326c3d7982e3f0c5f6d8955e3c705fd9da
Ognjen Stanisavljevic <ognjen.stanisavljevic@thingsolver.com>
2021-05-18 17:41:43 +0200

    premestanje ruta

--- register-api/src/main.py
+++ register-api/src/main.py
@@ -34,40 +34,6 @@ class Item_association(BaseModel):
     web : Optional[str] = None
     secret : str
 
-
-@app.post("/register-association", tags = ["Registracija udruzenja"])
-async def register_association(item : Item_association):
-    checkerica = CreateAssociation(item.email, item.username_association, item.secret).checker()
-    checkerica_email = Is_email_valid(item.email).check()
-    if checkerica_email['exist'] == True:
-        if checkerica['exist'] == False:
-            CreateAssociation(item.email, item.username_association, item.secret).new_association()
-            check_user_id = CreateAssociation(item.email, item.username_association, item.secret).get_keycloak_user_id()
-            CreateAssociation(item.email, item.username_association, item.secret).assign_keycloak_roles()
-            if check_user_id["exist"] == False:
-                print("association does not exist")
-            else:
-                CreateAssociation(item.email, item.username_association, item.secret).verify_email(check_user_id['user_id_keycloak'])
-                print("The email has been successfully sent!")
-            return {"message" : "The association has been successfully created!"}
-        else:
-            raise HTTPException(status_code = 409, detail = "Association already exists")
-    else:
-        raise HTTPException(status_code = 406, detail = "Email is not acceptable")
-        
-    
-
-
-@app.post("/resend-email", tags = ["Ponovo posalji email verifikaciju"])
-async def resend_email(item : Item_for_resend):
-    check_user_id_for_resend = Resend_verify_email(item.username).resend(item.username)
-    print(check_user_id_for_resend)
-    if check_user_id_for_resend['exist'] == False:
-        raise HTTPException(status_code = 404, detail = "username does not exist")
-    else:
-        Resend_verify_email(item.username).send(check_user_id_for_resend['user_id_keycloak'])
-        return {"message" : "The email has been successfully sent!"}
-
 @app.get("/health", tags = ["Provera rada aplikacije"])
 async def index():
     return {"HEALTH" : "OK"}
@@ -91,5 +57,36 @@ async def register_user(item:Item):
             raise HTTPException(status_code = 409, detail = "User already exists")
     else:
         raise HTTPException(status_code = 406, detail = "Email is not acceptable")
+
+@app.post("/register-association", tags = ["Registracija udruzenja"])
+async def register_association(item:Item_association):
+    checkerica = CreateAssociation(item.email, item.username_association, item.secret).checker()
+    checkerica_email = Is_email_valid(item.email).check()
+    if checkerica_email['exist'] == True:
+        if checkerica['exist'] == False:
+            CreateAssociation(item.email, item.username_association, item.secret).new_association()
+            check_user_id = CreateAssociation(item.email, item.username_association, item.secret).get_keycloak_user_id()
+            CreateAssociation(item.email, item.username_association, item.secret).assign_keycloak_roles()
+            if check_user_id["exist"] == False:
+                print("association does not exist")
+            else:
+                CreateAssociation(item.email, item.username_association, item.secret).verify_email(check_user_id['user_id_keycloak'])
+                print("The email has been successfully sent!")
+            return {"message" : "The association has been successfully created!"}
+        else:
+            raise HTTPException(status_code = 409, detail = "Association already exists")
+    else:
+        raise HTTPException(status_code = 406, detail = "Email is not acceptable")
+
+@app.post("/resend-email", tags = ["Ponovo posalji email verifikaciju"])
+async def resend_email(item : Item_for_resend):
+    check_user_id_for_resend = Resend_verify_email(item.username).resend(item.username)
+    print(check_user_id_for_resend)
+    if check_user_id_for_resend['exist'] == False:
+        raise HTTPException(status_code = 404, detail = "username does not exist")
+    else:
+        Resend_verify_email(item.username).send(check_user_id_for_resend['user_id_keycloak'])
+        return {"message" : "The email has been successfully sent!"}
+
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, loop="asyncio")
