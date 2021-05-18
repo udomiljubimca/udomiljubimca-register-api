e7c6fb9c4b5d83e97bf1a3da89e76ab5607d1be1
Ognjen Stanisavljevic <ognjen.stanisavljevic@thingsolver.com>
2021-05-18 21:06:46 +0200

    added .lower part for string in class

--- register-api/src/main.py
+++ register-api/src/main.py
@@ -16,22 +16,22 @@ logger.setLevel("DEBUG")
 app = FastAPI(openapi_tags = register_api_docs, openapi_prefix = "/api/latest/register-api")
 
 class ItemUser(BaseModel):
-    email : str
-    username : str
+    email : str.lower
+    username : str.lower
     firstName : str
     lastName : str
     secret: str
 
 class ItemForResend(BaseModel):
-    username : str
+    username : str.lower
 
 class ItemAssociation(BaseModel):
-    email : str
-    username : str
+    email : str.lower
+    username : str.lower
     number_association : Optional[int] = None
     place : Optional[str] = None
     phone_number : Optional[int] = None
-    web : Optional[str] = None
+    web : Optional[str.lower] = None
     secret : str
 
 @app.get("/health", tags = ["Provera rada aplikacije"])
