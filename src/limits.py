bf23156a06fa04082140306455f5fb604754c053
Mifa43 <mifa43kotez@gmail.com>
2021-07-31 12:00:00 +0200

    Kreirana je klasa koja proverava unose od korisnika

--- src/limits.py
+++ src/limits.py
@@ -1,14 +1,21 @@
-
-
 class LimitPassword():
     def __init__(self, password):
         self.password = password
     def is_password_valid(self):
-        pass
-
+        min = 8
+        max = 12
+        if len(password) >= min and len(password) <= max:
+            return {"status": True}
+        else:
+            return {"status": False}
 
 class LimitUsername():
     def __init__(self, username):
         self.username = username
     def is_username_valid(self):
-        pass
+        min = 5
+        max = 20
+        if len(username) >= min and len(username) <= max:
+            return {"status": True}
+        else:
+            return {"status": False}
