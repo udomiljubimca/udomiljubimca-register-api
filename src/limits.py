86188d590a0569d7f367e0045cbf7977a45d273b
Mifa43 <mifa43kotez@gmail.com>
2021-07-31 11:40:50 +0200

    Kreiranje dve nove klase koje ce da vrse proveru password-a i username-a

---
+++ src/limits.py
@@ -0,0 +1,14 @@
+
+
+class LimitPassword():
+    def __init__(self, password):
+        self.password = password
+    def is_password_valid(self):
+        pass
+
+
+class LimitUsername():
+    def __init__(self, username):
+        self.username = username
+    def is_username_valid(self):
+        pass
