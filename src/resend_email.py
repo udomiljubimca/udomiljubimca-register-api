8d987fed8b1d728b1c9c7997f63ab29890b5220a
Mifa43 <mifa43kotez@gmail.com>
2021-10-05 11:39:11 +0200

    dodata deskripcija i komentari za resend email verification

--- src/resend_email.py
+++ src/resend_email.py
@@ -4,10 +4,41 @@ import os
 
 
 class Resend_verify_email():
+    """
+    Klasa za kreiranje udruzenja sadrzi metode.
+
+    Parametri:
+    ---------------
+
+        username : str
+
+            : -> Uzima user input.
+
+    Variable:
+    ---------------
+
+        username : str
+
+            : -> Cuvanje user inputa i slanje na Keycloak.
+
+    Rezultat:
+    ---------------
+        : -> Resend email
+    """
     def __init__(self, username):
         self.username = username
 
     def resend(self, username):
+        """
+        Uzmi Keycloak user id.
+
+        Parametri:
+        ---------------
+
+            user_id_keycloak -> username : None -> bool(False) - ako nema username
+
+            user_id_keycloak -> username : bool(True), user_id - ima username
+        """
         keycloak_admin = KeycloakAdmin(server_url="{}/auth/".format(os.getenv('KEYCLOAK_URL')),
                                 username = os.getenv('KEYCLOAK_ADMIN_USER'),
                                 password = os.getenv('KEYCLOAK_ADMIN_PASSWORD'),
@@ -21,10 +52,17 @@ class Resend_verify_email():
             return {"exist" : True, "user_id_keycloak" : user_id_keycloak}
 
     def send(self, user_id_keycloak):
+        """ Ponovo posalji email verifikaciju.
+
+        user_id_keycloak -> send_verify_email
+        
+        send_verify_email : realm_name -> izvrsna komanda slanje verifikacije
+        """
         keycloak_admin = KeycloakAdmin(server_url="{}/auth/".format(os.getenv('KEYCLOAK_URL')),
                                 username = os.getenv('KEYCLOAK_ADMIN_USER'),
                                 password = os.getenv('KEYCLOAK_ADMIN_PASSWORD'),
                                 realm_name = "master",
                                 verify = True)
         keycloak_admin.realm_name = os.getenv('CLIENT_RELM_NAME')
+        # user_id_keycloak dobijamo iz prethodne funkcije koju saljemo iz main fajla
         keycloak_admin.send_verify_email(user_id=user_id_keycloak)
