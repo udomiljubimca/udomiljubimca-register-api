e9f878e43680cdc3decae01acb7c4162a12f1045
Mifa43 <mifa43kotez@gmail.com>
2021-10-05 11:27:18 +0200

    dodata deskripcija za limitiranje password-a i username-a

--- src/limits.py
+++ src/limits.py
@@ -1,7 +1,20 @@
 class LimitPassword():
+    """ Limitiranje password-a.
+
+    Parametri:
+    ---------------
+        password : str
+
+    Rezultat:
+    ---------------
+        password valid : -> True
+    """
     def __init__(self, password):
         self.password = password
     def is_password_valid(self):
+        """
+        Password je limitiran na minimalno 8 i maksimalno 12 karaktera.
+        """
         min = 8
         max = 12
         if len(self.password) >= min and len(self.password) <= max:
@@ -10,9 +23,22 @@ class LimitPassword():
             return {"status": False}
 
 class LimitUsername():
+    """ Limitiranje username-a.
+
+    Parametri:
+    ---------------
+        username : str
+
+    Rezultat:
+    ---------------
+        username valid : -> True
+    """
     def __init__(self, username):
         self.username = username
     def is_username_valid(self):
+        """
+        Username je limitiran na minimalno 5 i maksimalno 20 karaktera.
+        """
         min = 5
         max = 20
         if len(self.username) >= min and len(self.username) <= max:
